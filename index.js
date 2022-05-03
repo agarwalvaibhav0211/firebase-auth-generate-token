d83f469727b63acdd617f7a4003e052059c8581e
freeroamer90 <agarwalvaibhav0211@gmail.com>
2022-05-03 15:27:52 +0530

    Updated with latest versions

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const firebase = require('firebase');
+const { initializeApp } = require('firebase/app');
+const {getAuth,signInWithEmailAndPassword} = require("firebase/auth");
 const prompt = require('prompt');
 require('dotenv').config();
 
 // Initialize Firebase App.
-firebase.initializeApp({
+initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -28,17 +29,18 @@ prompt.get(
     let password = result.password;
 
     // Authenticate user using email and password.
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(response => {
+    var auth = getAuth();
+      signInWithEmailAndPassword(auth,email, password)
+        .then(async function(response) {
+          let idtoken = await auth.currentUser.getIdToken();
+          console.log(idtoken);
         // Get the access token of the authenticated user.
-        firebase
-          .auth()
-          .currentUser.getIdToken()
-          .then(token => {
-            console.log(token);
-          });
+        // firebase
+        //   .auth()
+        //   .currentUser.getIdToken()
+        //   .then(token => {
+        //     console.log(token);
+        //   });
       })
       .catch(error => {
         console.error(error);
